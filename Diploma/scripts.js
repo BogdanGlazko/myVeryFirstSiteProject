551abebaa924c1cc2981b261f1710bec5c153f42
BogdanGlazko <82181202+BogdanGlazko@users.noreply.github.com>
2021-09-26 13:44:22 +0200

    Add files via upload
    MyWork

---
+++ Diploma/scripts.js
@@ -0,0 +1,8 @@
+let myText;
+myText = "Вы на странице Active Trust!";
+alert(myText);
+
+
+function remindMe() {
+    document.getElementById("otpravka").innerHTML = "Форма успешно отправлена, с вами скоро свяжутся";
+  }
